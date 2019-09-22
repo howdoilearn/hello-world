c86365022924401f63e217de7b3c31e8405e2271
Ian <ianandveronicashomecomputer@ian.attlocal.net>
2019-09-22 11:35:20 -0700

    initial commit

--- tests/login_tests.py
+++ tests/login_tests.py
@@ -1,22 +1,21 @@
 import utils.utils as utils
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import inspect
 import pytest
 import allure
 import moment
-from allure.constants import AttachmentType
+from allure_commons.types import AttachmentType
 
 now = moment.now().strftime("%d-%m-%Y")
-
 
 def open_app(driver):
     try:
         driver.get(utils.APP)
     except:
         # Add screenshot to Allure report
-        allure.attach(inspect.currentframe().f_code.co_name + "-" + now, driver.get_screenshot_as_png(),
-                      type=AttachmentType.PNG)
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
         # Save screenshot locally
         utils.save_screenshot(driver, inspect.currentframe().f_code.co_name)
         raise
@@ -24,16 +23,16 @@ def open_app(driver):
 
 def login_negative(driver):
     try:
+        utils.click(driver, By.LINK_TEXT, utils.log_in_link)
         utils.type_text(driver, utils.USERNAME, By.NAME, utils.username_locator)
         utils.type_text(driver, utils.BAD_PASSWORD, By.NAME, utils.password_locator)
-        utils.click(driver, By.XPATH, utils.login_button)
+        utils.click(driver, By.ID, utils.login_button)
         time.sleep(3)
-        assert driver.find_element(By.CSS_SELECTOR, utils.error).is_displayed()
+        assert ('Invalid' in driver.page_source)
     except:
         # Add screenshot to Allure report
-        allure.attach(inspect.currentframe().f_code.co_name + "-" + now, driver.get_screenshot_as_png(),
-                      type=AttachmentType.PNG)
-        # Save screenshot locally
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+            # Save screenshot locally
         utils.save_screenshot(driver, inspect.currentframe().f_code.co_name)
         raise
 
@@ -42,13 +41,12 @@ def login_positive(driver):
     try:
         utils.type_text(driver, utils.USERNAME, By.NAME, utils.username_locator)
         utils.type_text(driver, utils.PASSWORD, By.NAME, utils.password_locator)
-        utils.click(driver, By.XPATH, utils.login_button)
+        utils.click(driver, By.ID, utils.login_button)
         time.sleep(3)
         assert driver.find_element(By.CSS_SELECTOR, utils.user_menu).is_displayed()
     except:
         # Add screenshot to Allure report
-        allure.attach(inspect.currentframe().f_code.co_name + "-" + now, driver.get_screenshot_as_png(),
-                      type=AttachmentType.PNG)
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
         # Save screenshot locally
         utils.save_screenshot(driver, inspect.currentframe().f_code.co_name)
         raise
@@ -60,8 +58,7 @@ def teardown_function(driver):
         driver.quit()
     except:
         # Add screenshot to Allure report
-        allure.attach(inspect.currentframe().f_code.co_name + "-" + now, driver.get_screenshot_as_png(),
-                      type=AttachmentType.PNG)
+        allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
         # Save screenshot locally
         utils.save_screenshot(driver, inspect.currentframe().f_code.co_name)
         raise
--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ import os
 import string
 
 # Constants
-now = moment.now().strftime("%d-%m-%Y")
+now = moment.now().strftime("%m/%d/%Y, %H:%M:%S")
 path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 screen_dir = os.path.join(path, "screenshot", str(now))
 
@@ -13,28 +13,29 @@ PASSWORD = "sandbox"
 BAD_PASSWORD = "1234"
 
 # Locators
+log_in_link = "log in"
 username_locator = "username"
 password_locator = "password"
-login_button = "//input[@value='Log in']"
+login_button = "loginbtn"
 user_menu = ".usermenu"
-error = '.error'
-
+error = '.Invalid'
 # Functions
 def screen_path():
     global screen_dir
     if not os.path.exists(screen_dir):
         os.makedirs(screen_dir)
-        os.chmod(screen_dir, 0755)
+        os.chmod(screen_dir, 0o755)
     return screen_dir
 
 
 def save_screenshot(driver, name):
-    _name = remove_special_characters(name)
+#    _name = remove_special_characters(name)
+    _name = ''
     driver.get_screenshot_as_file(os.path.join(screen_path(), _name + '-' + now + ".png"))
 
 
 def remove_special_characters(text):
-    return text.translate(string.maketrans('', ''), '\ / : * ? " < > |')
+    return text.translate(str.maketrans('', ''), '/ : * ? " < > |')
 
 
 def click(driver, *args):
